55990da685b041705ef3f9b8060acd7f68441689
Vanja Smiljkovic <vanjasmiljkovic22@gmail.com>
2021-01-08 19:32:08 +0100

    Make some changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,8 +3,8 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get() //home page, root nase aplikacije http://localhost:3000/
-  getHello(): string {
-    return 'Hello World!';
+  getIndex(): string {
+    return 'Home page!';
   }
 
   @Get('world') // http://localhost:3000/world
